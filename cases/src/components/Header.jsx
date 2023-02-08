b9f7c895d38cd8a2d0515832768bd9e105098f26
Naushad <17naushad2002@gmail.com>
2023-02-08 15:56:45 +0530

    few updates

--- cases/src/components/Header.jsx
+++ cases/src/components/Header.jsx
@@ -4,13 +4,16 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import ImageList from "@mui/material/ImageList";
 import LogoutIcon from "@mui/icons-material/Logout";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from '@mui/material/Avatar';
+import logo from "../Images/logo.png"
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -22,10 +25,11 @@ export default function Header() {
           >
             <MenuIcon />
           </IconButton>
+          <img alt="Logo" height="40px" src={logo} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Cases
           </Typography>
-          <LogoutIcon/>
+          <LogoutIcon />
         </Toolbar>
       </AppBar>
     </Box>
